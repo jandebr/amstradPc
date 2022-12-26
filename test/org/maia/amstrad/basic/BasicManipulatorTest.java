a1f66547bff49c84236732e57c7331df718d530f
jandebr <34720253+jandebr@users.noreply.github.com>
2022-12-26 22:19:45 +0100

    Scanning for goto self

--- test/org/maia/amstrad/basic/BasicManipulatorTest.java
+++ test/org/maia/amstrad/basic/BasicManipulatorTest.java
@@ -3,8 +3,12 @@ package org.maia.amstrad.basic;
 import java.io.File;
 
 import org.maia.amstrad.basic.locomotive.source.BasicSourceCode;
+import org.maia.amstrad.basic.locomotive.source.BasicSourceCodeLine;
+import org.maia.amstrad.basic.locomotive.source.LineNumberToken;
+import org.maia.amstrad.basic.locomotive.source.SourceToken;
 import org.maia.amstrad.basic.locomotive.source.SourceTokenFactory;
 import org.maia.amstrad.basic.locomotive.source.SourceTokenSequence;
+import org.maia.amstrad.io.AmstradFileType;
 import org.maia.amstrad.program.AmstradProgramException;
 
 public class BasicManipulatorTest {
@@ -13,6 +17,11 @@ public class BasicManipulatorTest {
 	}
 
 	public static void main(String[] args) throws BasicSyntaxException, AmstradProgramException {
+		// manipulate();
+		scanFilesInDirectory(new File("D:/tools/JavaCPC/programs"));
+	}
+
+	private static void manipulate() throws BasicSyntaxException, AmstradProgramException {
 		BasicProgramFile program = new BasicProgramFile(new File("resources/test/manipulator/test.bas"));
 		BasicSourceCode sourceCode = new BasicSourceCode(program.getPayload().asTextPayload().getText());
 		manipulate(sourceCode);
@@ -27,6 +36,51 @@ public class BasicManipulatorTest {
 		SourceTokenSequence sequence = new SourceTokenSequence();
 		sequence.append(stf.createLineNumber(ln + 5), stf.createBasicKeyword("END"));
 		sourceCode.addLine(sequence.assemble());
+	}
+
+	private static void scanFilesInDirectory(File dir) throws BasicSyntaxException, AmstradProgramException {
+		File[] files = dir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			File file = files[i];
+			if (file.isDirectory()) {
+				scanFilesInDirectory(file);
+			} else if (AmstradFileType.BASIC_SOURCE_CODE_FILE.matches(file)) {
+				scanFile(file);
+			}
+		}
+	}
+
+	private static void scanFile(File basicFile) throws BasicSyntaxException, AmstradProgramException {
+		System.out.println("Scanning " + basicFile.getPath() + "...");
+		BasicSourceCode sourceCode = new BasicSourceCode(
+				new BasicProgramFile(basicFile).getPayload().asTextPayload().getText());
+		scanSourceCode(sourceCode);
+		System.out.println();
+	}
+
+	private static void scanSourceCode(BasicSourceCode sourceCode) throws BasicSyntaxException {
+		scanSourceCodeForGotoSelf(sourceCode);
+	}
+
+	private static void scanSourceCodeForGotoSelf(BasicSourceCode sourceCode) throws BasicSyntaxException {
+		SourceToken gotoToken = SourceTokenFactory.getInstance().createBasicKeyword("GOTO");
+		SourceToken ifToken = SourceTokenFactory.getInstance().createBasicKeyword("IF");
+		for (BasicSourceCodeLine line : sourceCode) {
+			SourceTokenSequence sequence = line.parse();
+			if (!sequence.contains(ifToken)) {
+				int i = sequence.getFirstIndexOf(gotoToken);
+				while (i >= 0) {
+					i = sequence.getNextIndexOf(LineNumberToken.class, i + 1);
+					if (i >= 0) {
+						int ln = ((LineNumberToken) sequence.get(i)).getValue();
+						if (ln == line.getLineNumber()) {
+							System.out.println(line);
+						}
+						i = sequence.getNextIndexOf(gotoToken, i + 1);
+					}
+				}
+			}
+		}
 	}
 
 }
