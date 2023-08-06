76426ca0ed12855b044ef0221077bc9ebe297d93
jandebr <34720253+jandebr@users.noreply.github.com>
2023-08-06 12:41:08 +0200

    Recognizing alternative display sources by type

---
+++ src/org/maia/amstrad/pc/monitor/display/source/AmstradAlternativeDisplaySourceType.java
@@ -0,0 +1,15 @@
+package org.maia.amstrad.pc.monitor.display.source;
+
+public enum AmstradAlternativeDisplaySourceType {
+
+	PROGRAM_BROWSER,
+
+	PROGRAM_STANDALONE_INFO,
+
+	IMAGE,
+
+	TERMINATION,
+
+	OTHER;
+
+}
