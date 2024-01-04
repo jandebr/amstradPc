503b3e5d65702e3caa25493de88987267a52e2a7
jandebr <34720253+jandebr@users.noreply.github.com>
2024-01-04 18:46:38 +0100

    Remove unnecessary code

--- src/org/maia/amstrad/system/AmstradSystem.java
+++ src/org/maia/amstrad/system/AmstradSystem.java
@@ -62,11 +62,6 @@ public abstract class AmstradSystem {
 	protected abstract void doLaunch(AmstradPc amstradPc, String[] args) throws AmstradException;
 
 	protected void doPostStartupActions(AmstradPc amstradPc) {
-		if (getSystemSettings().isLaunchInFullscreen()) {
-			amstradPc.getMonitor().makeFullscreen();
-		} else {
-			amstradPc.getMonitor().makeWindowed();
-		}
 		notifyInitialScreen(getCurrentScreen());
 		// Subclasses may extend this method
 	}
